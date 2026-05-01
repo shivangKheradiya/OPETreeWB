c53ae896cb42a79d38b798f29bd98c3f7de2505a
Shivang Kheradiya <shivangatul@gmail.com>
2026-05-01 11:49:49 +0200

    Minor Change

---
+++ tests/deletePyCache.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import shutil
+
+def remove_pycache(root_dir):
+    for pycache in Path(root_dir).rglob("__pycache__"):
+        if pycache.is_dir():
+            shutil.rmtree(pycache)
+
+# Example usage
+remove_pycache(r"C:\SKRepo\OPETreeWB")  # current directory
